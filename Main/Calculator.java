afc227a4a93fd0ca3112c9f523aed6d66ddfb804
yongjun428 <igagima2@naver.com>
2024-10-28 05:15:57 +0900

    먼저 외관만 간단하게 디자인을 만듬

--- Main/Calculator.java
+++ Main/Calculator.java
@@ -1,12 +1,69 @@
 package Main;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class Calculator extends JFrame {
-    Calculator() {
-        setTitle("Calculator");
-        setSize(500, 500);
+    JTextField text;
 
+
+
+
+    Calculator() {
+        setTitle("계산기");
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        Case();
+        Result();
+        Num();
+
+        setVisible(true);
+
+    }
+    void Case() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        add(panel);
+    }
+
+    void Result() {
+        JPanel panelT = new JPanel();
+
+        text = new JTextField(10);
+        text.setEditable(false);
+
+        panelT.add(text);
+        add(text, BorderLayout.NORTH);
+
+    }
+    void Num() {
+        JPanel panelN = new JPanel();
+        panelN.setLayout(new GridLayout(4,4,2,2));
+        add(panelN, BorderLayout.CENTER);
+
+        String[] NUM = {
+                "7", "8", "9", "+",
+                "4", "5", "6", "-",
+                "1", "2", "3", "/",
+                "0", "=", "C", "*"
+        };
+
+        for (String label : NUM) {
+            JButton button = new JButton (label);
+
+            panelN.add(button);
+        }
+
+        setVisible(true);
+
+    }
+    public static void main(String[] args) {
+
+        new Calculator();
     }
 
 }
